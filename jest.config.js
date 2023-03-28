498ea96ea0603b214ea141e679fbf0c8c297575c
namrata.t <namrata.t.easternts@gmail.com>
2023-03-28 12:35:08 +0530

    vue school course

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,18 @@ module.exports = {
   verbose: true,
   roots: ["<rootDir>/src/", "<rootDir>/specs/"],
   moduleFileExtensions: ['js', 'vue'],
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+
   transform: {
     "^.+\\.js$": "babel-jest",
   },
+
   snapshotSerializers: [
     "<rootDir>/node_modules/jest-serializer-vue"
-  ]
+  ],
+
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
 }
